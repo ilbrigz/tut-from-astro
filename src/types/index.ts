e2f1574ab6cb79620dd18faebdb7eaee9d926962
ilogirb23 <ilogirb23@yahoo.com>
2024-06-27 17:07:51 +0800

    Revert "."
    This reverts commit e36fcfe3a752f071472d2a4d79c93a67e47bc29a.

--- src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export interface MarkdownInstance<T extends Record<string, any>> {
-  frontmatter: T;
-  file: string;
-  url: string | undefined;
-  content: any;
-  getHeadings(): Promise<{ depth: number; slug: string; text: string }[]>;
-}
